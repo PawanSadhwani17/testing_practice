76ff8eb702e898aade8af9974c83a76f173307fc
PawanSadhwani17 <84863380+PawanSadhwani17@users.noreply.github.com>
2022-12-13 21:24:31 +0530

    Update fabonacci.py

--- src/fabonacci.py
+++ src/fabonacci.py
@@ -1,6 +1,6 @@
 def Fibonacci(n):
-    if n < 0:
-        return "Incorrect input"
+#     if n < 0:
+#         return "Incorrect input"
     elif n == 0:
         return 0 
     elif n == 1 or n == 2:
